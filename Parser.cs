c64d0117776ec86764ca8478bd496448a9a7b62b
stuartbryce <stuart.l.bryce@gmail.com>
2017-12-04 08:49:18 +0000

    moved parsing logic to Parser from Program

--- Parser.cs
+++ Parser.cs
@@ -9,15 +9,42 @@ namespace Timezone
     class Parser : IParser
     {
         //adding implementation of DisplayTime as reqd by IParser interface
-        public void DisplayTime(string time, string timezone, string converted_time){
-            
+        public void DisplayTime(string time, string timezone, string converted_time)
+        {
+
             //The time in the UK is { time} and the time in { timezone} is { converted time}
-            Console.WriteLine("The time in the UK is {0} and the time in {1} is {2}", time, timezone, converted_time); 
+            Console.WriteLine("The time in the UK is {0} and the time in {1} is {2}", time, timezone, converted_time);
         }
 
         internal void DisplayTime(Tuple<string, string> timezone)
         {
-            throw new NotImplementedException();
+            //Lookup timezone  
+            TimeZoneInfo timeZoneInfo = TimezoneRetriever.Find(timezone.Item2);
+
+            //If timezone found, calculate converted time and display 
+            if (timeZoneInfo != null)
+            {
+                //Try to parse time - catch resultant errors 
+                try
+                {
+                    DateTime time = DateTime.Parse(timezone.Item1);
+                    String converted_time = (TimeZoneInfo.ConvertTimeFromUtc(time, timeZoneInfo)).ToString("HH:mm");
+                    //Print info to console
+                    Console.WriteLine("The time in the UK is {0} and the time in {1} is {2}", timezone.Item1, timezone.Item2, converted_time);
+
+                }
+                catch (FormatException)
+                {
+                    //If time value is not valid 
+                    Console.WriteLine("Error: invalid Time value");
+                }
+            }
+            else
+            {
+                //If no timezone found
+                Console.WriteLine("Error: Timezone not found");
+            }
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -21,32 +21,8 @@ namespace Timezone
                 //Loop through each time-timezone pair 
                 foreach (Tuple<string, string> entry in lTimes)
                 {
-
-                    //Lookup timezone  
-                    TimeZoneInfo timeZoneInfo = TimezoneRetriever.Find(entry.Item2);
-
-                    //If timezone found, calculate converted time and display 
-                    if (timeZoneInfo != null)
-                    {
-                        //Try to parse time - catch resultant errors 
-                        try
-                        {
-                            DateTime time = DateTime.Parse(entry.Item1);
-                            String converted_time = (TimeZoneInfo.ConvertTimeFromUtc(time, timeZoneInfo)).ToString("HH:mm");
-                            //Print info to console
-                            timeZoneParser.DisplayTime(entry.Item1, entry.Item2, converted_time);
-                        }
-                        catch (FormatException)
-                        {
-                            //If time value is not valid 
-                            Console.WriteLine("Error: invalid Time value");
-                        }
-                    }
-                    else
-                    {
-                        //If no timezone found
-                        Console.WriteLine("Error: Timezone not found");
-                    }
+                    timeZoneParser.DisplayTime(entry); 
+               
                 }
             }
         }
